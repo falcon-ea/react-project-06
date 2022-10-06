245eb1d029f74a24f297c3dfe168b2d77f3f427d
Evgeny <dffalconn@gmail.com>
2022-10-06 08:53:57 +0300

    refactor: Reroving a collection component to a separate file; getting collections from mockapi, filtering by collection names

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
+import {Collection} from './Collection'
 import './index.scss';
-
-function Collection({ name, images }) {
-  return (
-    <div className="collection">
-      <img className="collection__big" src={images[0]} alt="Item" />
-      <div className="collection__bottom">
-        <img className="collection__mini" src={images[1]} alt="Item" />
-        <img className="collection__mini" src={images[2]} alt="Item" />
-        <img className="collection__mini" src={images[3]} alt="Item" />
-      </div>
-      <h4>{name}</h4>
-    </div>
-  );
-}
+import {logDOM} from "@testing-library/react";
 
 function App() {
+  const [searchValue, setSearchValue] = React.useState('')
+  const [collections, setCollections] = React.useState([])
+
+  React.useEffect(() => {
+    fetch('https://633c0e74f11701a65f6dcf3f.mockapi.io/fotocollection')
+      .then((res) => res.json())
+      .then((data) => {
+        setCollections(data)
+      })
+      .catch(err => console.warn('Ошибка при получении данных: ', err))
+  }, [])
+
+
   return (
     <div className="App">
       <h1>Моя коллекция фотографий</h1>
@@ -27,18 +28,18 @@ function App() {
           <li>Архитектура</li>
           <li>Города</li>
         </ul>
-        <input className="search-input" placeholder="Поиск по названию" />
+        <input
+          value={searchValue}
+          onChange={event => setSearchValue(event.target.value)}
+          className="search-input"
+          placeholder="Поиск по названию"
+        />
       </div>
       <div className="content">
-        <Collection
-          name="Путешествие по миру"
-          images={[
-            'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1560840067-ddcaeb7831d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1531219572328-a0171b4448a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1573108724029-4c46571d6490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-          ]}
-        />
+        {collections.filter(obj => obj.name.toLowerCase().includes(searchValue.toLowerCase()))
+          .map((obj, index) => (
+          <Collection key={index} name={obj.name} images={obj.photos}/>
+        ))}
       </div>
       <ul className="pagination">
         <li>1</li>
